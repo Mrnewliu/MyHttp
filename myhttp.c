3e0e4bc15c8429df418bdb098f7c378c5d8bfc75
root <1204218465@qq.com>
2018-10-17 21:06:04 +0800

    update response

--- myhttp.c
+++ myhttp.c
@@ -2,7 +2,7 @@
 #include<string.h>
 #include<stdlib.h>
 #include<errno.h>
-
+#include<fcntl.h>
 #include<unistd.h>
 #include<pthread.h>
 
@@ -18,9 +18,12 @@ int s,sc[MAX_LISTEN];
 static pthread_t th[MAX_LISTEN];
 static int max_flag;
 char rbuf[10*K];
+char wbuf[10*K];
 static void Init(void);
 struct sockaddr_in server,client;
 static void* thread_fun(void* arg);
+static unsigned char respond_pack(void* buf);
+
 int main(int argc,char* argv[])
 {
     Init();
@@ -37,12 +40,12 @@ int main(int argc,char* argv[])
             printf("%s\n",strerror(errno));
             continue;
         }
-        
+
         if( pthread_create(&th[i], NULL,thread_fun,(void*)i) != 0)
         {
             printf("phtread create %d failed,errno is %d\n",i, errno);
         }
-        i++; 
+        i++;
 
         if(i > MAX_LISTEN)
         {
@@ -71,7 +74,7 @@ static void Init(void)
     server.sin_port = htons(54188);
     server.sin_addr.s_addr = htonl(INADDR_ANY);
     //server.sin_addr.s_addr = inet_addr("47.89.8.236");
-    
+
 
 
     if(bind(s,(struct sockaddr*)(&server),sizeof(server)) == -1)
@@ -91,7 +94,7 @@ static void* thread_fun(void* arg)
     max_flag++;
     while(1)
     {
-        num = read(sc[i],rbuf,sizeof(rbuf));
+        num = recv(sc[i],rbuf,sizeof(rbuf),0);
         if( num < 0 )
         {
             printf("read finish\n");
@@ -100,8 +103,46 @@ static void* thread_fun(void* arg)
         else
         {
             printf("%s\n",rbuf);
+            respond_pack(wbuf);
+            num = send(sc[i],wbuf,strlen(wbuf),0);
+            if(num < 0)
+            {
+                printf("send error,errno is %d\n",errno);
+                sleep(1);
+                break;
+            }
         }
     }
     max_flag--;
     pthread_exit((void*)11);
+}
+static unsigned char respond_pack(void* buf)
+{
+    int fd,num = 8;
+    char datafile[2*K];
+
+    bzero(buf,sizeof(buf));
+
+    if( (fd = open("www/index.html",O_RDWR)) == -1)
+    {
+        printf("open failed,errno is %d\n",errno);
+        exit(-1);
+    }
+    do{
+        num = read(fd,datafile,124);
+        if(num < 0)
+        {
+            printf("read error,errorno is %d\n",errno);
+            continue;
+        }
+    }while(num == 124);
+    
+    close(fd);
+
+    sprintf(buf,
+    "HTTP/1.1  OK\r\n"
+    "Conten-Type:text/html:charset-iso--8b59-1\r\n"
+    "%s\r\n",datafile);
+    
+    return 0;
 }
